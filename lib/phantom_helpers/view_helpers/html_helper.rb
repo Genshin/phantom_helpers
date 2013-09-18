8b1e331964a01fa2a16f326781b85fec22c0b003
snowsunny <snowsunny1107@gmail.com>
2013-09-19 04:46:05 +0900

    added panel_div

--- lib/phantom_helpers/view_helpers/html_helper.rb
+++ lib/phantom_helpers/view_helpers/html_helper.rb
@@ -20,6 +20,32 @@ module PhantomHelpers
         end
       end
 
+      def panel_div(color = 'default', title = nil, footer = nil, &block)
+        content_tag :div, class: "row" do
+          content_tag :div, class: "col-md-12" do
+            content_tag :div, class: "panel panel-#{color}" do
+              [
+                if title
+                  content_tag :div, class: "panel-heading" do
+                    content_tag :h3, class: "panel-title" do
+                      title
+                    end
+                  end
+                end,
+                content_tag(:div, class: "panel-body") do
+                  block.call
+                end,
+                if footer
+                  content_tag :div, class: "panel-footer" do
+                    footer
+                  end
+                end
+              ].join.html_safe
+            end
+          end
+        end
+      end
+
       def index_body(&block)
         content_tag :div, class: "row" do
           content_tag :div, class: "col-md-12" do

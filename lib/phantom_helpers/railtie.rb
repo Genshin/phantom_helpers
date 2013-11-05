9e56f82f175e702dab330cc5b7be32151050507e
Vassil Kalkov <vassilkalkov@gmail.com>
2013-11-05 11:27:43 +0200

    Add css

--- lib/phantom_helpers/railtie.rb
+++ lib/phantom_helpers/railtie.rb
@@ -1,6 +1,9 @@
 require 'phantom_helpers/view_helpers'
 module PhantomHelpers
   class Railtie < Rails::Railtie
+
+    config.assets.paths << File.expand_path("../../../vendor/assets/stylesheets", __FILE__)
+
     initializer "phantom_helpers" do
       I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/../../locales'), '*.yml')]
       I18n.load_path.flatten!
